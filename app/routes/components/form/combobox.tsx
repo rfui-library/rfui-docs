41704c3e414506323bbb2503fb78d0ea7856324b
Adam Zerner <adamzerner@protonmail.com>
2025-01-29 00:15:53 -0800

    Add docs for a bunch of stuff

--- app/routes/components/form/combobox.tsx
+++ app/routes/components/form/combobox.tsx
@@ -420,342 +420,342 @@ export default () => {
     //   ]}
     // />`,
     //     },
-    //     {
-    //       title: "Rounded",
-    //       description: (
-    //         <div>
-    //           Set <InlineCode>rounded</InlineCode> to either{" "}
-    //           <InlineCode>"square"</InlineCode>, <InlineCode>"sm"</InlineCode>,{" "}
-    //           <InlineCode>"lg"</InlineCode>, or <InlineCode>"full"</InlineCode>.
-    //           Defaults to the value of the CSS variable{" "}
-    //           <InlineCode>--default-roundedness</InlineCode>. See{" "}
-    //           <Link href="/default-roundedness">"Default roundedness"</Link>.
-    //         </div>
-    //       ),
-    //       demo: (
-    //         <Stack className="w-fit gap-5">
-    //           <Combobox
-    //             rounded="square"
-    //             options={[
-    //               {
-    //                 id: "foo",
-    //                 value: "foo",
-    //                 display: "foo",
-    //               },
-    //               {
-    //                 id: "bar",
-    //                 value: "bar",
-    //                 display: "bar",
-    //               },
-    //               {
-    //                 id: "baz",
-    //                 value: "baz",
-    //                 display: "baz",
-    //               },
-    //             ]}
-    //           />
-    //           <Combobox
-    //             rounded="sm"
-    //             options={[
-    //               {
-    //                 id: "foo",
-    //                 value: "foo",
-    //                 display: "foo",
-    //               },
-    //               {
-    //                 id: "bar",
-    //                 value: "bar",
-    //                 display: "bar",
-    //               },
-    //               {
-    //                 id: "baz",
-    //                 value: "baz",
-    //                 display: "baz",
-    //               },
-    //             ]}
-    //           />
-    //           <Combobox
-    //             rounded="lg"
-    //             options={[
-    //               {
-    //                 id: "foo",
-    //                 value: "foo",
-    //                 display: "foo",
-    //               },
-    //               {
-    //                 id: "bar",
-    //                 value: "bar",
-    //                 display: "bar",
-    //               },
-    //               {
-    //                 id: "baz",
-    //                 value: "baz",
-    //                 display: "baz",
-    //               },
-    //             ]}
-    //           />
-    //           <Combobox
-    //             rounded="full"
-    //             options={[
-    //               {
-    //                 id: "foo",
-    //                 value: "foo",
-    //                 display: "foo",
-    //               },
-    //               {
-    //                 id: "bar",
-    //                 value: "bar",
-    //                 display: "bar",
-    //               },
-    //               {
-    //                 id: "baz",
-    //                 value: "baz",
-    //                 display: "baz",
-    //               },
-    //             ]}
-    //           />
-    //         </Stack>
-    //       ),
-    //       code: `<Stack className="w-fit gap-5">
-    //   <Combobox
-    //     rounded="square"
-    //     options={[
-    //       {
-    //         id: "foo",
-    //         value: "foo",
-    //         display: "foo",
-    //       },
-    //       {
-    //         id: "bar",
-    //         value: "bar",
-    //         display: "bar",
-    //       },
-    //       {
-    //         id: "baz",
-    //         value: "baz",
-    //         display: "baz",
-    //       },
-    //     ]}
-    //   />
-    //   <Combobox
-    //     rounded="sm"
-    //     options={[
-    //       {
-    //         id: "foo",
-    //         value: "foo",
-    //         display: "foo",
-    //       },
-    //       {
-    //         id: "bar",
-    //         value: "bar",
-    //         display: "bar",
-    //       },
-    //       {
-    //         id: "baz",
-    //         value: "baz",
-    //         display: "baz",
-    //       },
-    //     ]}
-    //   />
-    //   <Combobox
-    //     rounded="lg"
-    //     options={[
-    //       {
-    //         id: "foo",
-    //         value: "foo",
-    //         display: "foo",
-    //       },
-    //       {
-    //         id: "bar",
-    //         value: "bar",
-    //         display: "bar",
-    //       },
-    //       {
-    //         id: "baz",
-    //         value: "baz",
-    //         display: "baz",
-    //       },
-    //     ]}
-    //   />
-    //   <Combobox
-    //     rounded="full"
-    //     options={[
-    //       {
-    //         id: "foo",
-    //         value: "foo",
-    //         display: "foo",
-    //       },
-    //       {
-    //         id: "bar",
-    //         value: "bar",
-    //         display: "bar",
-    //       },
-    //       {
-    //         id: "baz",
-    //         value: "baz",
-    //         display: "baz",
-    //       },
-    //     ]}
-    //   />
-    // </Stack>`,
-    //     },
-    //     {
-    //       title: "Disabled",
-    //       description: (
-    //         <div>
-    //           Set <InlineCode>disabled</InlineCode> to <InlineCode>true</InlineCode>
-    //           .
-    //         </div>
-    //       ),
-    //       demo: (
-    //         <Combobox
-    //           disabled
-    //           options={[
-    //             {
-    //               id: "foo",
-    //               value: "foo",
-    //               display: "foo",
-    //             },
-    //             {
-    //               id: "bar",
-    //               value: "bar",
-    //               display: "bar",
-    //             },
-    //             {
-    //               id: "baz",
-    //               value: "baz",
-    //               display: "baz",
-    //             },
-    //           ]}
-    //         />
-    //       ),
-    //       code: `<Combobox
-    //   disabled
-    //   options={[
-    //     {
-    //       id: "foo",
-    //       value: "foo",
-    //       display: "foo",
-    //     },
-    //     {
-    //       id: "bar",
-    //       value: "bar",
-    //       display: "bar",
-    //     },
-    //     {
-    //       id: "baz",
-    //       value: "baz",
-    //       display: "baz",
-    //     },
-    //   ]}
-    // />`,
-    //     },
-    //     {
-    //       title: "Disabled option",
-    //       description: (
-    //         <div>
-    //           Set <InlineCode>disabled</InlineCode> to <InlineCode>true</InlineCode>{" "}
-    //           for a given option .
-    //         </div>
-    //       ),
-    //       demo: (
-    //         <Combobox
-    //           options={[
-    //             {
-    //               id: "foo",
-    //               value: "foo",
-    //               display: "foo",
-    //               disabled: false,
-    //             },
-    //             {
-    //               id: "bar",
-    //               value: "bar",
-    //               display: "bar",
-    //               disabled: true,
-    //             },
-    //             {
-    //               id: "baz",
-    //               value: "baz",
-    //               display: "baz",
-    //               disabled: false,
-    //             },
-    //           ]}
-    //         />
-    //       ),
-    //       code: `<Combobox
-    //   options={[
-    //     {
-    //       id: "foo",
-    //       value: "foo",
-    //       display: "foo",
-    //       disabled: false,
-    //     },
-    //     {
-    //       id: "bar",
-    //       value: "bar",
-    //       display: "bar",
-    //       disabled: true,
-    //     },
-    //     {
-    //       id: "baz",
-    //       value: "baz",
-    //       display: "baz",
-    //       disabled: false,
-    //     },
-    //   ]}
-    // />`,
-    //     },
-    //     {
-    //       title: "Invalid",
-    //       description: (
-    //         <div>
-    //           Set <InlineCode>invalid</InlineCode> to
-    //           <InlineCode>true</InlineCode>.
-    //         </div>
-    //       ),
-    //       demo: (
-    //         <Combobox
-    //           invalid
-    //           options={[
-    //             {
-    //               id: "foo",
-    //               value: "foo",
-    //               display: "foo",
-    //             },
-    //             {
-    //               id: "bar",
-    //               value: "bar",
-    //               display: "bar",
-    //             },
-    //             {
-    //               id: "baz",
-    //               value: "baz",
-    //               display: "baz",
-    //             },
-    //           ]}
-    //         />
-    //       ),
-    //       code: `<Combobox
-    //   invalid
-    //   options={[
-    //     {
-    //       id: "foo",
-    //       value: "foo",
-    //       display: "foo",
-    //     },
-    //     {
-    //       id: "bar",
-    //       value: "bar",
-    //       display: "bar",
-    //     },
-    //     {
-    //       id: "baz",
-    //       value: "baz",
-    //       display: "baz",
-    //     },
-    //   ]}
-    // />`,
-    //     },
+    {
+      title: "Rounded",
+      description: (
+        <div>
+          Set <InlineCode>rounded</InlineCode> to either{" "}
+          <InlineCode>"square"</InlineCode>, <InlineCode>"sm"</InlineCode>,{" "}
+          <InlineCode>"lg"</InlineCode>, or <InlineCode>"full"</InlineCode>.
+          Defaults to the value of the CSS variable{" "}
+          <InlineCode>--default-roundedness</InlineCode>. See{" "}
+          <Link href="/default-roundedness">"Default roundedness"</Link>.
+        </div>
+      ),
+      demo: (
+        <Stack className="w-fit gap-5">
+          <Combobox
+            rounded="square"
+            options={[
+              {
+                id: "foo",
+                value: "foo",
+                display: "foo",
+              },
+              {
+                id: "bar",
+                value: "bar",
+                display: "bar",
+              },
+              {
+                id: "baz",
+                value: "baz",
+                display: "baz",
+              },
+            ]}
+          />
+          <Combobox
+            rounded="sm"
+            options={[
+              {
+                id: "foo",
+                value: "foo",
+                display: "foo",
+              },
+              {
+                id: "bar",
+                value: "bar",
+                display: "bar",
+              },
+              {
+                id: "baz",
+                value: "baz",
+                display: "baz",
+              },
+            ]}
+          />
+          <Combobox
+            rounded="lg"
+            options={[
+              {
+                id: "foo",
+                value: "foo",
+                display: "foo",
+              },
+              {
+                id: "bar",
+                value: "bar",
+                display: "bar",
+              },
+              {
+                id: "baz",
+                value: "baz",
+                display: "baz",
+              },
+            ]}
+          />
+          <Combobox
+            rounded="full"
+            options={[
+              {
+                id: "foo",
+                value: "foo",
+                display: "foo",
+              },
+              {
+                id: "bar",
+                value: "bar",
+                display: "bar",
+              },
+              {
+                id: "baz",
+                value: "baz",
+                display: "baz",
+              },
+            ]}
+          />
+        </Stack>
+      ),
+      code: `<Stack className="w-fit gap-5">
+  <Combobox
+    rounded="square"
+    options={[
+      {
+        id: "foo",
+        value: "foo",
+        display: "foo",
+      },
+      {
+        id: "bar",
+        value: "bar",
+        display: "bar",
+      },
+      {
+        id: "baz",
+        value: "baz",
+        display: "baz",
+      },
+    ]}
+  />
+  <Combobox
+    rounded="sm"
+    options={[
+      {
+        id: "foo",
+        value: "foo",
+        display: "foo",
+      },
+      {
+        id: "bar",
+        value: "bar",
+        display: "bar",
+      },
+      {
+        id: "baz",
+        value: "baz",
+        display: "baz",
+      },
+    ]}
+  />
+  <Combobox
+    rounded="lg"
+    options={[
+      {
+        id: "foo",
+        value: "foo",
+        display: "foo",
+      },
+      {
+        id: "bar",
+        value: "bar",
+        display: "bar",
+      },
+      {
+        id: "baz",
+        value: "baz",
+        display: "baz",
+      },
+    ]}
+  />
+  <Combobox
+    rounded="full"
+    options={[
+      {
+        id: "foo",
+        value: "foo",
+        display: "foo",
+      },
+      {
+        id: "bar",
+        value: "bar",
+        display: "bar",
+      },
+      {
+        id: "baz",
+        value: "baz",
+        display: "baz",
+      },
+    ]}
+  />
+</Stack>`,
+    },
+    {
+      title: "Disabled",
+      description: (
+        <div>
+          Set <InlineCode>disabled</InlineCode> to <InlineCode>true</InlineCode>
+          .
+        </div>
+      ),
+      demo: (
+        <Combobox
+          disabled
+          options={[
+            {
+              id: "foo",
+              value: "foo",
+              display: "foo",
+            },
+            {
+              id: "bar",
+              value: "bar",
+              display: "bar",
+            },
+            {
+              id: "baz",
+              value: "baz",
+              display: "baz",
+            },
+          ]}
+        />
+      ),
+      code: `<Combobox
+  disabled
+  options={[
+    {
+      id: "foo",
+      value: "foo",
+      display: "foo",
+    },
+    {
+      id: "bar",
+      value: "bar",
+      display: "bar",
+    },
+    {
+      id: "baz",
+      value: "baz",
+      display: "baz",
+    },
+  ]}
+/>`,
+    },
+    {
+      title: "Disabled option",
+      description: (
+        <div>
+          Set <InlineCode>disabled</InlineCode> to <InlineCode>true</InlineCode>{" "}
+          for a given option .
+        </div>
+      ),
+      demo: (
+        <Combobox
+          options={[
+            {
+              id: "foo",
+              value: "foo",
+              display: "foo",
+              disabled: false,
+            },
+            {
+              id: "bar",
+              value: "bar",
+              display: "bar",
+              disabled: true,
+            },
+            {
+              id: "baz",
+              value: "baz",
+              display: "baz",
+              disabled: false,
+            },
+          ]}
+        />
+      ),
+      code: `<Combobox
+  options={[
+    {
+      id: "foo",
+      value: "foo",
+      display: "foo",
+      disabled: false,
+    },
+    {
+      id: "bar",
+      value: "bar",
+      display: "bar",
+      disabled: true,
+    },
+    {
+      id: "baz",
+      value: "baz",
+      display: "baz",
+      disabled: false,
+    },
+  ]}
+/>`,
+    },
+    {
+      title: "Invalid",
+      description: (
+        <div>
+          Set <InlineCode>invalid</InlineCode> to
+          <InlineCode>true</InlineCode>.
+        </div>
+      ),
+      demo: (
+        <Combobox
+          invalid
+          options={[
+            {
+              id: "foo",
+              value: "foo",
+              display: "foo",
+            },
+            {
+              id: "bar",
+              value: "bar",
+              display: "bar",
+            },
+            {
+              id: "baz",
+              value: "baz",
+              display: "baz",
+            },
+          ]}
+        />
+      ),
+      code: `<Combobox
+  invalid
+  options={[
+    {
+      id: "foo",
+      value: "foo",
+      display: "foo",
+    },
+    {
+      id: "bar",
+      value: "bar",
+      display: "bar",
+    },
+    {
+      id: "baz",
+      value: "baz",
+      display: "baz",
+    },
+  ]}
+/>`,
+    },
   ];
   const propsTables = [
     {
