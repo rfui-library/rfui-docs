7587e5b94ae9cda4ebe4a3162ce2493ec1571ca7
Adam Zerner <adamzerner@protonmail.com>
2025-10-28 11:53:47 -0700

    Make demos more realistic

--- app/routes/components/form/form-layout.tsx
+++ app/routes/components/form/form-layout.tsx
@@ -2,7 +2,13 @@ import {
   ComponentDocsPage,
   type ExampleType,
 } from "@/components/component-docs-page/index";
-import { FormField, FormLayout, FormSection, Input } from "rfui-package";
+import {
+  FormField,
+  FormLayout,
+  FormSection,
+  InlineCode,
+  Input,
+} from "rfui-package";
 
 export default () => {
   const overviewNotes = null;
@@ -13,18 +19,53 @@ export default () => {
         <FormLayout>
           <FormSection
             title="Personal information"
-            description="This is the personal information section"
+            description="Please provide your personal details"
           >
-            <FormField label="Name">
-              <Input name="name" />
+            <FormField label="First name">
+              <Input name="firstName" />
+            </FormField>
+            <FormField label="Last name">
+              <Input name="lastName" />
+            </FormField>
+            <FormField label="Date of birth">
+              <Input name="dateOfBirth" type="date" />
+            </FormField>
+            <FormField label="Phone number">
+              <Input name="phone" type="tel" />
+            </FormField>
+          </FormSection>
+          <FormSection
+            title="Address"
+            description="Where do you currently reside?"
+          >
+            <FormField label="Street address">
+              <Input name="streetAddress" />
+            </FormField>
+            <FormField label="City">
+              <Input name="city" />
+            </FormField>
+            <FormField label="State / Province">
+              <Input name="state" />
+            </FormField>
+            <FormField label="ZIP / Postal code">
+              <Input name="zipCode" />
             </FormField>
           </FormSection>
           <FormSection
             title="Contact information"
-            description="This is the contact information section"
+            description="How can we reach you?"
           >
             <FormField label="Email">
-              <Input name="email" />
+              <Input name="email" type="email" />
+            </FormField>
+            <FormField label="Alternative email">
+              <Input name="alternativeEmail" type="email" />
+            </FormField>
+            <FormField label="Emergency contact name">
+              <Input name="emergencyContactName" />
+            </FormField>
+            <FormField label="Emergency contact phone">
+              <Input name="emergencyContactPhone" type="tel" />
             </FormField>
           </FormSection>
         </FormLayout>
@@ -32,18 +73,171 @@ export default () => {
       code: `<FormLayout>
   <FormSection
     title="Personal information"
-    description="This is the personal information section"
+    description="Please provide your personal details"
   >
-    <FormField label="Name">
-      <Input name="name" />
+    <FormField label="First name">
+      <Input name="firstName" />
+    </FormField>
+    <FormField label="Last name">
+      <Input name="lastName" />
+    </FormField>
+    <FormField label="Date of birth">
+      <Input name="dateOfBirth" type="date" />
+    </FormField>
+    <FormField label="Phone number">
+      <Input name="phone" type="tel" />
+    </FormField>
+  </FormSection>
+  <FormSection
+    title="Address"
+    description="Where do you currently reside?"
+  >
+    <FormField label="Street address">
+      <Input name="streetAddress" />
+    </FormField>
+    <FormField label="City">
+      <Input name="city" />
+    </FormField>
+    <FormField label="State / Province">
+      <Input name="state" />
+    </FormField>
+    <FormField label="ZIP / Postal code">
+      <Input name="zipCode" />
     </FormField>
   </FormSection>
   <FormSection
     title="Contact information"
-    description="This is the contact information section"
+    description="How can we reach you?"
   >
     <FormField label="Email">
-      <Input name="email" />
+      <Input name="email" type="email" />
+    </FormField>
+    <FormField label="Alternative email">
+      <Input name="alternativeEmail" type="email" />
+    </FormField>
+    <FormField label="Emergency contact name">
+      <Input name="emergencyContactName" />
+    </FormField>
+    <FormField label="Emergency contact phone">
+      <Input name="emergencyContactPhone" type="tel" />
+    </FormField>
+  </FormSection>
+</FormLayout>`,
+    },
+    {
+      title: "Vertical layout",
+      description: (
+        <div>
+          The default layout is horizontal but if you pass{" "}
+          <InlineCode>layout="vertical"</InlineCode>, the sections will be
+          displayed vertically.
+        </div>
+      ),
+      demo: (
+        <FormLayout layout="vertical">
+          <FormSection
+            title="Personal information"
+            description="Please provide your personal details"
+          >
+            <FormField label="First name">
+              <Input name="firstName" />
+            </FormField>
+            <FormField label="Last name">
+              <Input name="lastName" />
+            </FormField>
+            <FormField label="Date of birth">
+              <Input name="dateOfBirth" type="date" />
+            </FormField>
+            <FormField label="Phone number">
+              <Input name="phone" type="tel" />
+            </FormField>
+          </FormSection>
+          <FormSection
+            title="Address"
+            description="Where do you currently reside?"
+          >
+            <FormField label="Street address">
+              <Input name="streetAddress" />
+            </FormField>
+            <FormField label="City">
+              <Input name="city" />
+            </FormField>
+            <FormField label="State / Province">
+              <Input name="state" />
+            </FormField>
+            <FormField label="ZIP / Postal code">
+              <Input name="zipCode" />
+            </FormField>
+          </FormSection>
+          <FormSection
+            title="Contact information"
+            description="How can we reach you?"
+          >
+            <FormField label="Email">
+              <Input name="email" type="email" />
+            </FormField>
+            <FormField label="Alternative email">
+              <Input name="alternativeEmail" type="email" />
+            </FormField>
+            <FormField label="Emergency contact name">
+              <Input name="emergencyContactName" />
+            </FormField>
+            <FormField label="Emergency contact phone">
+              <Input name="emergencyContactPhone" type="tel" />
+            </FormField>
+          </FormSection>
+        </FormLayout>
+      ),
+      code: `<FormLayout layout="vertical">
+  <FormSection
+    title="Personal information"
+    description="Please provide your personal details"
+  >
+    <FormField label="First name">
+      <Input name="firstName" />
+    </FormField>
+    <FormField label="Last name">
+      <Input name="lastName" />
+    </FormField>
+    <FormField label="Date of birth">
+      <Input name="dateOfBirth" type="date" />
+    </FormField>
+    <FormField label="Phone number">
+      <Input name="phone" type="tel" />
+    </FormField>
+  </FormSection>
+  <FormSection
+    title="Address"
+    description="Where do you currently reside?"
+  >
+    <FormField label="Street address">
+      <Input name="streetAddress" />
+    </FormField>
+    <FormField label="City">
+      <Input name="city" />
+    </FormField>
+    <FormField label="State / Province">
+      <Input name="state" />
+    </FormField>
+    <FormField label="ZIP / Postal code">
+      <Input name="zipCode" />
+    </FormField>
+  </FormSection>
+  <FormSection
+    title="Contact information"
+    description="How can we reach you?"
+  >
+    <FormField label="Email">
+      <Input name="email" type="email" />
+    </FormField>
+    <FormField label="Alternative email">
+      <Input name="alternativeEmail" type="email" />
+    </FormField>
+    <FormField label="Emergency contact name">
+      <Input name="emergencyContactName" />
+    </FormField>
+    <FormField label="Emergency contact phone">
+      <Input name="emergencyContactPhone" type="tel" />
     </FormField>
   </FormSection>
 </FormLayout>`,
